1f867f88a43f04764852c5ef8c9f8d221fa3c5b5
Ha Junyoung <jun19973@naver.com>
2026-05-21 16:37:02 +0900

    [feat]: 유저 회원가입 구현, [chore] : docker 데이터를 db에 저장할 수 있게 volums 설정 수정

--- init.sql
+++ init.sql
@@ -1,3 +1,5 @@
--- 데이터베이스 초기 구동 시 스키마 공간을 자동으로 미리 생성하는 스크립트
+-- 1. 유저 서비스 (userservice) 전용 공간
 CREATE SCHEMA IF NOT EXISTS user_schema;
+
+-- 2. 배송 담당자 서비스 (deliverymanagerservice) 전용 공간
 CREATE SCHEMA IF NOT EXISTS delivery_manager_schema;
