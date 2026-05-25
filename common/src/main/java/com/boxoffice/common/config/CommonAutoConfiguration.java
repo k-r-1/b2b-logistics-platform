cdb28fd1f10a7e3f6988f349efbeb3ba20d5cc5c
Ha Junyoung <jun19973@naver.com>
2026-05-25 22:43:26 +0900

    feat: [2/3] User 핵심 로직 및 공통 모듈 리팩토링 #21

---
+++ common/src/main/java/com/boxoffice/common/config/CommonAutoConfiguration.java
@@ -0,0 +1,9 @@
+package com.boxoffice.common.config;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@AutoConfiguration
+@ComponentScan(basePackages = "com.boxoffice.common")
+public class CommonAutoConfiguration {
+}
