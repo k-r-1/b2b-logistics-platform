b287b412ff8da7b41ae1539cb683cf3da6c5456f
GolemOnce <shh3350@gmail.com>
2026-05-27 14:39:32 +0900

    chore: kafka 환경 세팅

---
+++ delivery-service/src/main/java/boxoffice/deliveryservice/kafka/config/KafkaConfig.java
@@ -0,0 +1,35 @@
+package boxoffice.deliveryservice.kafka.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaConfig {
+
+    @Value("${kafka.topic.hub-transfer-dispatched}")
+    private String hubTransferDispatched;
+
+    @Value("${kafka.topic.transfer-assign-success}")
+    private String transferAssignSuccess;
+
+    @Value("${kafka.topic.transfer-assign-failed}")
+    private String transferAssignFailed;
+
+    @Bean
+    public NewTopic hubTransferDispatchedTopic() {
+        return TopicBuilder.name(hubTransferDispatched).partitions(3).replicas(1).build();
+    }
+
+    @Bean
+    public NewTopic transferAssignSuccessTopic() {
+        return TopicBuilder.name(transferAssignSuccess).partitions(3).replicas(1).build();
+    }
+
+    @Bean
+    public NewTopic transferAssignFailedTopic() {
+        return TopicBuilder.name(transferAssignFailed).partitions(3).replicas(1).build();
+    }
+}
