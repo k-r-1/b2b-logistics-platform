8c74d6d7565fe6384183737b39d40de76c399e42
GolemOnce <shh3350@gmail.com>
2026-05-28 15:22:20 +0900

    fix: 병합 과정에서 생긴 import 누락 해결

--- delivery-service/src/main/java/boxoffice/deliveryservice/domain/delivery/service/DeliveryService.java
+++ delivery-service/src/main/java/boxoffice/deliveryservice/domain/delivery/service/DeliveryService.java
@@ -2,6 +2,7 @@ package boxoffice.deliveryservice.domain.delivery.service;
 
 import boxoffice.deliveryservice.client.HubClient;
 import boxoffice.deliveryservice.client.UserServiceClient;
+import boxoffice.deliveryservice.client.dto.response.HubRouteResponseDto;
 import boxoffice.deliveryservice.client.dto.response.UserInfoDto;
 import boxoffice.deliveryservice.domain.delivery.dto.request.DeliveryCreateRequestDto;
 import boxoffice.deliveryservice.domain.delivery.dto.response.DeliveryResponseDto;
