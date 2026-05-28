3185f30715a660209daab05cd2e881ca66adb296
GolemOnce <shh3350@gmail.com>
2026-05-28 11:34:30 +0900

    refactor: AddressRequest > AddressVO 변경

--- delivery-service/src/main/java/boxoffice/deliveryservice/domain/delivery/dto/request/DeliveryCreateRequestDto.java
+++ delivery-service/src/main/java/boxoffice/deliveryservice/domain/delivery/dto/request/DeliveryCreateRequestDto.java
@@ -4,7 +4,6 @@ import com.boxoffice.common.entity.AddressVO;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 import java.util.UUID;
 
@@ -12,17 +11,7 @@ public record DeliveryCreateRequestDto(
         @NotNull UUID orderId,
         @NotNull UUID originHubId,
         @NotNull UUID destinationHubId,
-        @Valid @NotNull AddressRequest deliveryAddress,
+        @Valid @NotNull AddressVO deliveryAddress,
         @NotBlank String recipientName,
         String recipientSlackId
-) {
-    public record AddressRequest(
-            @Size(max = 10) String zipCode,
-            @NotBlank String address,
-            @Size(max = 255) String detailAddress
-    ) {
-        public AddressVO toAddressVO() {
-            return new AddressVO(zipCode, address, detailAddress);
-        }
-    }
-}
+) {}
